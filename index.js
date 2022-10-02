f1cb2c305a3e5c8498db8d801f1402290e0cc432
Mahmoud-M-Hamdan <eng.mech.hamdan@gmail.com>
2022-10-03 01:55:42 +0300

    3

--- index.js
+++ index.js
@@ -1,23 +1,20 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
-const port = process.env.PORT || 3000;
 // const User = require("./model/user");
 const jwt = require("jsonwebtoken");
 
-require('dotenv').config()
-console.log(process.env.MONGOOSE_URL) 
+require("dotenv").config();
 // const Auth = require("./middleware/auth");
-const userRouter=require('./routes/user')
-const taskRouter=require('./routes/task')
+const userRouter = require("./routes/user");
+const taskRouter = require("./routes/task");
 mongoose.connect(process.env.MONGOOSE_URL);
 app.use(express.json());
-app.use(userRouter)
-app.use(taskRouter)
-app.listen(port, () => {
-  console.log(`here I am on ${port}`);
+app.use(userRouter);
+app.use(taskRouter);
+app.listen(process.env.PORT || 3000, () => {
+  console.log(`here I am on ${process.env.PORT}`);
 });
-app.listen(port)
 app.get("/", (req, res) => {
   res.send("Hello from another world");
 });
